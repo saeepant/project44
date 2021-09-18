d61c1929b961c07c9dacefbd7b8317a817b031e6
saeepant <73944687+saeepant@users.noreply.github.com>
2021-09-18 17:20:07 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,14 @@
+class Game{
+    constructor(){
+
+    }
+
+    start(){
+// fill write color of choice
+fill("black");
+        text("Welcome to the Game - Swim with Music. Click on Play" , displayWidth/3 , 200);
+      
+
+
+    }
+}
